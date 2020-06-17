ef7a3e25845820f0c80dff42c19bb3b08f86133d
caronoff <caronoff@gmail.com>
2020-06-17 12:35:15 -0400

    initial

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ load_dotenv('.env')
 
 app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY', 'Optional default value')
-gmap_key = os.getenv('GMAP_KEY', 'Optional default value')
+
 
 COUNTRIES=[]
 country=open('countries2.csv')
