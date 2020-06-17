374d2f248f61f182aaf8120d2267ef1b8fa98b02
caronoff <caronoff@gmail.com>
2020-06-17 12:33:07 -0400

    initial

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-from flask import Flask,flash,jsonify,request,render_template,redirect,url_for
+from flask import Flask,jsonify,request
 import decodehex2
 import re
 import os
-from decodefunctions import is_number
 from dotenv import load_dotenv
 load_dotenv('.env')
 
@@ -38,7 +37,7 @@ def validatehex():
             new_data=new_data.upper()
     return jsonify(echostatus=statuscheck, message=message,newdata=new_data)
 
-@app.route('/api/<hexcode>',methods=['GET'])
+@app.route('/<hexcode>',methods=['GET'])
 def api(hexcode):
     try:
         beacon = decodehex2.Beacon(hexcode)
@@ -46,33 +45,7 @@ def api(hexcode):
         return jsonify(error=[err.value,err.message])
     return jsonify(result=beacon.tablebin,mid=beacon.get_mid(),country=beacon.get_country(),msgtype=beacon.type,tac=beacon.gettac(), beacontype=beacon.btype(),protocol=beacon.protocoltype())
 
-@app.route("/decoded/<hexcode>",methods=['GET','POST'])
-def decoded(hexcode):
-    geocoord = (0, 0)
-    locationcheck = False
-    try:
-        beacon = decodehex2.Beacon(hexcode)
-        if beacon.has_loc() and is_number(beacon.location[0]) and is_number(beacon.location[1]):
-            geocoord = (float(beacon.location[0]),float(beacon.location[1]))
-            locationcheck=True
 
-        return render_template('output.html', hexcode=hexcode.upper(),
-                               decoded=beacon.tablebin,
-                               locationcheck=locationcheck,
-                               geocoord=geocoord,
-                               genmsg=beacon.genmsg,
-                               uin = beacon.hexuin(),
-                               gmap_key=gmap_key)
-    except decodehex2.HexError as err:
-        print(err.value,err.message)
-        return render_template('badhex.html',errortype=err.value,errormsg=err.message)
-
-@app.route("/",methods=['GET','POST'])
-def decode():
-    if request.method == 'POST':
-        hexcode = str(request.form['hexcode']).strip()
-        return redirect(url_for('decoded',hexcode=hexcode))
-    return render_template('decodehex.html', title='Home')
 
 
 if __name__ == '__main__':
