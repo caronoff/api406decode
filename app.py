dde0e740b0a0a8130d5de3cf8d8fb4c43dbffd9f
caronoff <caronoff@gmail.com>
2020-07-22 14:07:25 -0400

    initial

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask,jsonify,request
 import decodehex2
-import re
+import boto3
 import os
 from dotenv import load_dotenv
 load_dotenv('.env')
+
+USERS_TABLE = os.environ['USERS_TABLE']
+client = boto3.client('dynamodb')
 
 app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY', 'Optional default value')
@@ -33,6 +36,47 @@ def decode(hexcode):
 def api():
     msg='hello world 4'
     return jsonify(msg=msg)
+
+
+@app.route("/users/<string:user_id>")
+def get_user(user_id):
+    resp = client.get_item(
+        TableName=USERS_TABLE,
+        Key={
+            'userId': { 'S': user_id }
+        }
+    )
+    item = resp.get('Item')
+    if not item:
+        return jsonify({'error': 'User does not exist'}), 404
+
+    return jsonify({
+        'userId': item.get('userId').get('S'),
+        'name': item.get('name').get('S')
+    })
+
+
+@app.route("/users", methods=["POST"])
+def create_user():
+    user_id = request.json.get('userId')
+    name = request.json.get('name')
+    if not user_id or not name:
+        return jsonify({'error': 'Please provide userId and name'}), 400
+
+    resp = client.put_item(
+        TableName=USERS_TABLE,
+        Item={
+            'userId': {'S': user_id },
+            'name': {'S': name }
+        }
+    )
+
+    return jsonify({
+        'userId': user_id,
+        'name': name
+    })
+
+
 
 
 
